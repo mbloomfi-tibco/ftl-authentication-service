8a7cc6bf1ecc05ba647d2e925346cd041860a5de
Mark Bloomfield <mbloomfi@tibco.com>
2022-09-30 11:19:39 +0200

    Updated with SSL configuration.  Added logging approach and removed Spring Boot LDAP code.

--- src/main/java/com/tibco/messaging/ftl/authservice/controller/FTLLoginController.java
+++ src/main/java/com/tibco/messaging/ftl/authservice/controller/FTLLoginController.java
@@ -1,14 +1,13 @@
 package com.tibco.messaging.ftl.authservice.controller;
 
-import com.tibco.messaging.ftl.authservice.ldap.SimpleLDAPClient;
-import com.tibco.messaging.ftl.authservice.ldap.client.LdapClient;
+import com.tibco.messaging.ftl.authservice.ldap.client.SimpleLDAPClient;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 
-@RestController
+@Controller
 public class LoginController {
 
         //   LdapClient ldapClient = new LdapClient();
@@ -21,31 +20,41 @@ public class LoginController {
                 System.out.println("password:" + authRequest.getPassword());
                 System.out.println("userName:" + authRequest.getUsername());
                 System.out.println("realm" + authRequest.getMeta().getRealm());
-                System.out.println("appname");
+
+                System.out.println("password-dec:" + authRequest.getPasswordDec());
+
                 FtlAuthenticationResponse ftlAuthenticationResponse = new FtlAuthenticationResponse();
 
+                ArrayList roles = new ArrayList<String>();
 
-           /**     if (authRequest.getPassword().compareTo("ZnJlZA==") == 0) {
+                //Use this block of code to do a simple check and if you don't have LDAP.  You will need ot comment out the LDAP Section
+
+               /**     if (authRequest.getPassword().compareTo("ZnJlZA==") == 0) {
                         ftlAuthenticationResponse.setAuthenticated(true);
                 } else {
                         ftlAuthenticationResponse.setAuthenticated(false);
                 }
-           **/
+                **/
+
                 try {
-                     ldapClient.authenticateUser(authRequest.getUsername(), authRequest.getPassword());
-                        ftlAuthenticationResponse.setAuthenticated(true);
+                          ftlAuthenticationResponse.setAuthenticated(ldapClient.authenticateUser(authRequest.getUsername(),authRequest.getPasswordDec()));
+
                 } catch  (Exception ex){
                         System.out.println("Failed to log in user " + authRequest.getPassword());
                         ex.printStackTrace();
                         ftlAuthenticationResponse.setAuthenticated(false);
                 }
+
+                if (ftlAuthenticationResponse.isAuthenticated()) {
+                      //  roles.add("ftl-internal");
+                        roles.add("ftl-admin");
+                       //   roles.add("ftl");
+                        roles.add("ftl-guest");
+                        roles.add("ftl-admin");
+
+                }
+
                 // LDAP Query..  Map ftl-admin .. EMS Admin Group..
-
-                ArrayList roles = new ArrayList<String>();
-
-                roles.add("ftl-internal");
-                roles.add("ftl-admin");
-                roles.add("ftl");
 
                 ftlAuthenticationResponse.setRoles(roles);
 
---
+++ src/main/java/com/tibco/messaging/ftl/authservice/controller/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.tibco.messaging.ftl.authservice.controller;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize ->
+                        authorize.anyRequest().permitAll())
+                .build();
+    }
+
+}
